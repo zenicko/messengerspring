5eb6f1d8e5a02e3a2c16dbcb7979ebde66783e82
Nick Zemskoy <nzemskoy@gmail.com>
2022-03-18 21:55:29 +0300

    Start the Spring project: The simple messenger

---
+++ src/main/java/ru/zenicko/messengerspring/MessengerspringApplication.java
@@ -0,0 +1,13 @@
+package ru.zenicko.messengerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zenicko/messengerspring/MessengerspringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zenicko.messengerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
